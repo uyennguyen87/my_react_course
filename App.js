094875dcb537bbc2cdc83796dedb8dda63f90b90
uyen_nguyen <uyen.nguyen@axonactive.vn>
2016-02-22 14:45:15 +0700

    Hello your name, a first component

--- App.js
+++ App.js
@@ -1,8 +1,16 @@
 import React from 'react';
-class App extends React.Component {
-  render() {
-    return <div>Hello, Uyen</div>
-  }
-}
+// class App extends React.Component {
+//   render() {
+//     return <div>Hello, Uyen</div>
+//   }
+// }
+
+// class App extends React.Component {
+//   render() {
+//     return React.createElement('div', null, 'Hello Uyen')
+//   }
+// }
+
+const App = () => <h1>Hello Uyen</h1>
 
 export default App
