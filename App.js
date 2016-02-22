81ff946d75d2c15f5857e64e79f7899f56f55699
uyen_nguyen <uyen.nguyen@axonactive.vn>
2016-02-22 15:03:51 +0700

    Introduce properties

--- App.js
+++ App.js
@@ -1,16 +1,24 @@
 import React from 'react';
-// class App extends React.Component {
-//   render() {
-//     return <div>Hello, Uyen</div>
-//   }
-// }
+import ReactDOM from 'react-dom'
+class App extends React.Component {
+  render() {
+    let txt = this.props.txt
+    return <h1>{txt}</h1>
+  }
+}
 
-// class App extends React.Component {
-//   render() {
-//     return React.createElement('div', null, 'Hello Uyen')
-//   }
-// }
+// const App = () => <h1>Hello Uyen</h1>
 
-const App = () => <h1>Hello Uyen</h1>
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+}
 
-export default App
+App.defaultProps = {
+  txt: 'this is the default txt'
+}
+
+ReactDOM.render(
+  <App cat={5} />,
+  document.getElementById('app')
+);
