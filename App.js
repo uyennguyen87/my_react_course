ffcdca42c61e7f2c52e0ec07406c7bdcddd95f71
uyen_nguyen <uyen.nguyen@axonactive.vn>
2016-02-23 13:57:40 +0700

    using Mixin example

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import LifeCycle from './components/LifeCycle'
 import ChildrentRender from './components/ChildrentRender'
 import Refs from './components/Refs'
+import MixinComponent from './components/MixinComponent'
 
 class App extends React.Component {
   constructor(){
@@ -10,6 +11,7 @@ class App extends React.Component {
     this.renderLifeCycle = this.renderLifeCycle.bind(this)
     this.renderChildrentRender = this.renderChildrentRender.bind(this)
     this.renderRefs = this.renderRefs.bind(this)
+    this.renderMixinComponent = this.renderMixinComponent.bind(this)
     this.destroyDOMNode = this.destroyDOMNode.bind(this)
   }
 
@@ -34,6 +36,13 @@ class App extends React.Component {
     )
   }
 
+  renderMixinComponent(){
+    ReactDOM.render(
+      <MixinComponent />,
+      document.getElementById('a')
+    )
+  }
+
   destroyDOMNode(){
     ReactDOM.unmountComponentAtNode(document.getElementById('a'))
   }
@@ -47,6 +56,7 @@ class App extends React.Component {
           <li><button onClick={this.renderLifeCycle}>Render "LifeCycle"</button><i>   enable Debug Mode to see result</i></li>
           <li><button onClick={this.renderChildrentRender}>Render "ChildrentRender"</button></li>
           <li><button onClick={this.renderRefs}>Render "Refs"</button></li>
+          <li><button onClick={this.renderMixinComponent}>Render "MixinComponent"</button></li>
           <br />
           <li><button onClick={this.destroyDOMNode}>Destroy DOM Node</button></li>
         </ul>
